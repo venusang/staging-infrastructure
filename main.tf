402c64578f5427575bf3929779ad88faec603d81
Venus Ang <venusang@users.noreply.github.com>
2026-02-06 17:42:40 -0800

    Add changes to test Needs Attention status

--- main.tf
+++ main.tf
@@ -7,10 +7,21 @@ terraform {
 
 # Example: Random resource to demonstrate the workspace is working
 resource "random_pet" "example" {
-  length = 2
+  length = 3  # Changed from 2 to 3 - this will trigger a replacement
+}
+
+# Add another resource to show multiple changes
+resource "random_string" "example" {
+  length  = 16
+  special = false
 }
 
 output "example_pet_name" {
   value       = random_pet.example.id
   description = "A randomly generated pet name"
+}
+
+output "example_string" {
+  value       = random_string.example.result
+  description = "A random string"
 }
