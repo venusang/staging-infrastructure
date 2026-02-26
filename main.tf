53cd88cf19cb028edf775a4ca14cf09dada88e26
Venus Ang <venusang@users.noreply.github.com>
2026-02-26 08:35:02 -0800

    Run #71: Add random_id for run 71

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Staging Infrastructure - Error Test Run
+# Staging Infrastructure - Run #71
 terraform {
   required_version = ">= 1.5.0"
 }
@@ -7,7 +7,14 @@ resource "random_pet" "example" {
   length = 3
 }
 
-# This will cause an error
-output "broken_ref" {
-  value = random_pet.nonexistent.id
+resource "random_id" "run_71" {
+  byte_length = 8
+}
+
+output "run_number" {
+  value = "71"
+}
+
+output "run_id_71" {
+  value = random_id.run_71.hex
 }
