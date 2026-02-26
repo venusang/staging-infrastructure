818644bb0ef43cf772b8a0f488edd8f494ea0769
Venus Ang <venusang@users.noreply.github.com>
2026-02-26 08:32:53 -0800

    Run #3: Add random_id for run 3

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Staging Infrastructure - Run #2
+# Staging Infrastructure - Run #3
 terraform {
   required_version = ">= 1.5.0"
 }
@@ -7,14 +7,14 @@ resource "random_pet" "example" {
   length = 3
 }
 
-resource "random_id" "run_2" {
+resource "random_id" "run_3" {
   byte_length = 8
 }
 
 output "run_number" {
-  value = "2"
+  value = "3"
 }
 
-output "run_id_2" {
-  value = random_id.run_2.hex
+output "run_id_3" {
+  value = random_id.run_3.hex
 }
