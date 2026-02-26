9c23cc43aa04bf5f49f55f0aa3e4d5497acfe7e9
Venus Ang <venusang@users.noreply.github.com>
2026-02-26 08:32:50 -0800

    Run #1: Add random_id for run 1

--- main.tf
+++ main.tf
@@ -1,59 +1,20 @@
-# Staging Infrastructure
-# This is a simple example - add your actual infrastructure resources here
-
-# Staging Infrastructure
-# This is a simple example - add your actual infrastructure resources here
-
+# Staging Infrastructure - Run #1
 terraform {
   required_version = ">= 1.5.0"
 }
 
-# Example: Random resource to demonstrate the workspace is working
 resource "random_pet" "example" {
-  length = 4  # Changed from 3 to 4
+  length = 3
 }
 
-# Add another resource
-resource "random_string" "example" {
-  length  = 16
-  special = false
-}
-
-# New resource for testing multiple runs
-resource "random_id" "test1" {
+resource "random_id" "run_1" {
   byte_length = 8
 }
 
-# Another test resource
-resource "random_uuid" "test2" {
+output "run_number" {
+  value = "1"
 }
 
-# One more test resource
-resource "random_password" "test3" {
-  length  = 16
-  special = true
+output "run_id_1" {
+  value = random_id.run_1.hex
 }
-
-output "example_pet_name" {
-  value       = random_pet.example.id
-  description = "A randomly generated pet name"
-}
-
-output "example_string" {
-  value       = random_string.example.result
-  description = "A random string"
-}
-
-output "test1_id" {
-  value = random_id.test1.hex
-}
-
-output "test2_uuid" {
-  value = random_uuid.test2.result
-}
-
-output "test3_password" {
-  value     = random_password.test3.result
-  sensitive = true
-}
-
