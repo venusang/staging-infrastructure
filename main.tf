3d29a7a3fe886398100f49e5dce40db2c0130bac
Venus Ang <venusang@users.noreply.github.com>
2026-02-26 08:34:04 -0800

    Run #40: Error test - broken reference

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Staging Infrastructure - Run #39
+# Staging Infrastructure - Error Test Run
 terraform {
   required_version = ">= 1.5.0"
 }
@@ -7,14 +7,7 @@ resource "random_pet" "example" {
   length = 3
 }
 
-resource "random_id" "run_39" {
-  byte_length = 8
-}
-
-output "run_number" {
-  value = "39"
-}
-
-output "run_id_39" {
-  value = random_id.run_39.hex
+# This will cause an error
+output "broken_ref" {
+  value = random_pet.nonexistent.id
 }
